b08b665c6b7a5030301f835d537fabace62d62ba
Shrey Gupta <63344738+Gupta-Shrey855@users.noreply.github.com>
2022-06-26 20:13:17 +0530

    Add files via upload

---
+++ reacttodo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Testtodo from './components/testtodo';
+import Todo from "./components/todo";
+
+const App = () => {
+  return (
+    <>
+     <Todo />
+    </>
+  )
+}
+
+export default App
